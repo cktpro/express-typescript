ec39520a400da7941cbefa4c59d1b58dfe77cfcd
cktpro <kimtramcap@gmail.com>
2023-08-28 18:42:48 +0700

    first commmit

---
+++ data-source.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+import 'reflect-metadata';
+
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'mssql',
+  host: '113.160.224.121',
+  port: 1433,
+  username: 'developer',
+  password: 'developer',
+  database: 'TypeOrm',
+  entities: ['entities/**/*.entity{.ts,.js}', 'entities/**/*.schema{.ts,.js}'],
+  synchronize: true,
+  logging: false,
+  options: {
+    encrypt: false,
+  },
+});
